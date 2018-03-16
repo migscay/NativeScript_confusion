df16dd5036ffef340e91ebe4e2ec95a32e26a34d
Miguel Santiago <popeye.santiago@gmail.com>
2018-03-17 09:20:20 +1100

    assign 3 WIP

--- conFusion/app/dishdetail/dishdetail.component.ts
+++ conFusion/app/dishdetail/dishdetail.component.ts
@@ -13,6 +13,14 @@ import { ModalDialogService, ModalDialogOptions } from "nativescript-angular/mod
 import { CommentComponent } from "../comment/comment.component";
 import { Validators, FormBuilder, FormGroup} from '@angular/forms';
 import { ObservableArray } from 'tns-core-modules/data/observable-array';
+import { Page } from "ui/page";
+import { Animation, AnimationDefinition } from "ui/animation";
+import { View } from "ui/core/view";
+import * as gestures from "tns-core-modules/ui/gestures";
+import { SwipeGestureEventData, SwipeDirection } from "ui/gestures";
+import { Color } from 'color';
+import * as enums from "ui/enums";
+
 
 @Component({
   selector: 'app-dishdetail',
@@ -31,11 +39,17 @@ export class DishdetailComponent implements OnInit {
   addcomment: FormGroup;
   newComment = <Comment>{};
   comments: ObservableArray<Comment>;
+  showComments: boolean = false;
+
+  cardImage: View;
+  commentList: View;
+  cardLayout: View;
 
   constructor(private dishservice: DishService,
     private favoriteservice: FavoriteService,
     private route: ActivatedRoute,
     private formBuilder: FormBuilder, 
+    private page: Page, 
     private fonticon: TNSFontIconService,
     private routerExtensions: RouterExtensions,
     private _modalService: ModalDialogService, 
@@ -70,7 +84,7 @@ export class DishdetailComponent implements OnInit {
     if (!this.favorite) {
       console.log('Adding to Favorites', this.dish.id);
       this.favorite = this.favoriteservice.addFavorite(this.dish.id);
-      const toast = new Toasty("Added Dish "+ this.dish.id, "short", "bottom");
+      const toast = new Toasty(this.dish.name + " added to Favourite Dishes", "short", "bottom");
       toast.show();
     }
   }
@@ -120,4 +134,97 @@ export class DishdetailComponent implements OnInit {
       }        
     });
   }
+
+  onSwipe(args: SwipeGestureEventData) {
+
+    console.log('Inside Oswipe Direction ', args.direction);
+
+    if (this.dish) {
+      this.cardImage = <View>this.page.getViewById<View>("cardImage");
+      this.cardLayout = <View>this.page.getViewById<View>("cardLayout");
+      this.commentList = <View>this.page.getViewById<View>("commentList");
+
+      if (args.direction === SwipeDirection.up && !this.showComments ) {
+        this.animateUp();
+      }
+      else if (args.direction === SwipeDirection.down && this.showComments ) {
+        this.showComments = false;
+        this.animateDown();
+      }
+    }
+
+  }
+
+  showAndHideComments() {
+      this.cardImage = <View>this.page.getViewById<View>("cardImage");
+      this.cardLayout = <View>this.page.getViewById<View>("cardLayout");
+      this.commentList = <View>this.page.getViewById<View>("commentList");
+
+      if (!this.showComments ) {
+        this.animateUp();
+      }
+      else if (this.showComments ) {
+        this.showComments = false;
+        this.animateDown();
+      }
+  }
+
+  animateUp() {
+    let definitions = new Array<AnimationDefinition>();
+    let a1: AnimationDefinition = {
+        target: this.cardImage,
+        scale: { x: 1, y: 0 },
+        translate: { x: 0, y: -200 },
+        opacity: 0,
+        duration: 500,
+        curve: enums.AnimationCurve.easeIn
+    };
+    definitions.push(a1);
+
+    let a2: AnimationDefinition = {
+        target: this.cardLayout,
+        backgroundColor: new Color("#ffc107"),
+        duration: 500,
+        curve: enums.AnimationCurve.easeIn
+    };
+    definitions.push(a2);
+
+    let animationSet = new Animation(definitions);
+
+    animationSet.play().then(() => {
+      this.showComments = true;
+    })
+    .catch((e) => {
+        console.log(e.message);
+    });
+  } 
+
+  animateDown() {
+    let definitions = new Array<AnimationDefinition>();
+    let a1: AnimationDefinition = {
+        target: this.cardImage,
+        scale: { x: 1, y: 1 },
+        translate: { x: 0, y: 0 },
+        opacity: 1,
+        duration: 500,
+        curve: enums.AnimationCurve.easeIn
+    };
+    definitions.push(a1);
+
+    let a2: AnimationDefinition = {
+        target: this.cardLayout,
+        backgroundColor: new Color("#ffffff"),
+        duration: 500,
+        curve: enums.AnimationCurve.easeIn
+    };
+    definitions.push(a2);
+
+    let animationSet = new Animation(definitions);
+
+    animationSet.play().then(() => {
+    })
+    .catch((e) => {
+        console.log(e.message);
+    });
+  } 
 }
--- conFusion/app/reservation/reservation.component.ts
+++ conFusion/app/reservation/reservation.component.ts
@@ -5,19 +5,32 @@ import { Switch } from 'ui/switch';
 import { Validators, FormBuilder, FormGroup} from '@angular/forms';
 import { ModalDialogService, ModalDialogOptions } from "nativescript-angular/modal-dialog";
 import { ReservationModalComponent } from "../reservationmodal/reservationmodal.component";
-
+import { Page } from "ui/page";
+import { Animation, AnimationDefinition } from "ui/animation";
+import { View } from "ui/core/view";
+import { SwipeGestureEventData, SwipeDirection } from "ui/gestures";
+import { Color } from 'color';
+import * as enums from "ui/enums";
+import { Reservation } from '../shared/reservation';
 
 @Component({
     selector: 'app-reservation',
     moduleId: module.id,
-    templateUrl: './reservation.component.html'
+    templateUrl: './reservation.component.html',
+    styleUrls: ['./reservation.component.css']
 })
 export class ReservationComponent extends DrawerPage implements OnInit {
 
     reservation: FormGroup;
+    confirmReservation: boolean = false;
+    reserveConfirmation: Reservation;
+
+    reserveForm: View;
+    confirmation: View;  
 
     constructor(private changeDetectorRef: ChangeDetectorRef,
         private formBuilder: FormBuilder, 
+        private page: Page,
         private _modalService: ModalDialogService, 
         private vcRef: ViewContainerRef) { 
             super(changeDetectorRef);
@@ -26,8 +39,10 @@ export class ReservationComponent extends DrawerPage implements OnInit {
                 guests: 3,
                 smoking: false,
                 dateTime: ['', Validators.required]
-            });
-    }
+            });        
+
+            this.reserveConfirmation = null;
+        }
 
     ngOnInit() {
 
@@ -57,6 +72,53 @@ export class ReservationComponent extends DrawerPage implements OnInit {
 
     onSubmit() {
         console.log(JSON.stringify(this.reservation.value));
+        this.confirmReservation = true;
+
+        this.reserveForm = <View>this.page.getViewById<View>("reserveForm");
+        //this.confirmation = <View>this.page.getViewById<View>("confirmation");
+
+        let definitions = new Array<AnimationDefinition>();
+
+        let a1: AnimationDefinition = {
+            target: this.reserveForm,
+            scale: { x: 0, y: 0 },
+            //translate: { x: 250, y: 250 },
+            //opacity: 0,
+            //delay: 5000,
+            duration: 2500,
+            curve: enums.AnimationCurve.linear
+        };
+        definitions.push(a1);
+    
+        let a2: AnimationDefinition = {
+            target: this.confirmation,
+            scale: { x: 1, y: 1},
+            //translate: { x: 0, y: 0 },
+            //opacity: 1,
+            delay: 2500,
+            duration: 5000,
+            curve: enums.AnimationCurve.linear
+        };
+        //definitions.push(a2);
+    
+        let animationSet = new Animation(definitions);
+
+        //this.confirmation.animate({scale: { x: 1, y: 1}, duration: 3000});
+
+        animationSet.play().then(() => {
+          //this.reserveForm.className="confirmation"; 
+          //this.confirmReservation = true;
+          //this.reserveForm.animate({scale: { x: 1, y: 1}, duration: 3000});
+        })
+        .catch((e) => {
+            console.log(e.message);
+        });
+
+        this.reserveForm.className="confirmation"; 
+        this.reserveForm.animate({scale: { x: 1, y: 1}, delay: 3000, duration: 3000});
+        this.confirmReservation = true;
+
+
     }
 
     createModalView(args) {
